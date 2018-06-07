fcb60ea96eba1029e6ee327ebea6911bf290e974
Luke Stanislaus <luke@stanislaus.co.uk>
2018-06-07 21:36:43 +0100

    Imported excel write code

--- Sailing2/Sailing2/Program.cs
+++ Sailing2/Sailing2/Program.cs
@@ -7,13 +7,48 @@ using System.Windows.Input;
 using System.Linq;
 using System.Data;
 using ExcelDataReader;
+using System.Data.OleDb;
 
 namespace Sailing
 {
 
     class Program
     { 
-        
+        public static void ExcelWriteFile(string path, string name, string boat, int boatNumber)
+        {
+            File.Delete(path);
+            var connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + path + @"\Race List.txt" + ";Extended Properties=Excel 8.0";
+            using (var excelConnection = new OleDbConnection(connectionString))
+            {
+                // The excel file does not need to exist, opening the connection will create the
+                // excel file for you
+                if (excelConnection.State != ConnectionState.Open) { excelConnection.Open(); }
+
+                // data is an object so it works with DBNull.Value
+                object propertyOneValue = "coolhgh!";
+                object propertyTwoValue = "testing";
+
+                var sqlText = "CREATE TABLE YourTableNameHere ([PropertyOne] VARCHAR(100), [PropertyTwo] VARCHAR(100))";
+
+                // Executing this command will create the worksheet inside of the workbook
+                // the table name will be the new worksheet name
+                using (var command = new OleDbCommand(sqlText, excelConnection)) { command.ExecuteNonQuery(); }
+
+                // Add (insert) data to the worksheet
+                var commandText = $"Insert Into YourTableNameHere ([PropertyOne], [PropertyTwo]) Values (@PropertyOne, @PropertyTwo)";
+
+                using (var command = new OleDbCommand(commandText, excelConnection))
+                {
+                    // We need to allow for nulls just like we would with
+                    // sql, if your data is null a DBNull.Value should be used
+                    // instead of null 
+                    command.Parameters.AddWithValue("@PropertyOne", propertyOneValue ?? DBNull.Value);
+                    command.Parameters.AddWithValue("@PropertyTwo", propertyTwoValue ?? DBNull.Value);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
         //public static Dictionary<string, Boats> LoadFullExcel(string path)
         public static void LoadFullExcel(string path)
         {
